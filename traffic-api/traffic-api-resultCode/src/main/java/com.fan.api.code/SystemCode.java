207f085e384394542b2e6d7dc9727955b89cae53
fan <909483306@qq.com>
2022-11-21 23:27:36 +0800

    新增删除、修改用到的错误码

--- traffic-api/traffic-api-resultCode/src/main/java/com.fan.api.code/SystemCode.java
+++ traffic-api/traffic-api-resultCode/src/main/java/com.fan.api.code/SystemCode.java
@@ -11,15 +11,28 @@ public interface SystemCode {
     /**
      * 错误  提示  警告
      */
+    //系统通用
+    String TRAFFIC_SYSTEM_SUCCESS = "000000";//系统通用成功状态码
+    String TRAFFIC_SYSTEM_ERROR = "999999";//系统通用失败状态码
 
     //用户管理		10000 - 10999
+    //添加
     String SYSTEM_USER_ERROR_ADD_FAIL = "10000"; //用户添加失败
     String SYSTEM_USER_ERROR_ADD_FAIL_PARAM_NULL = "10001"; //请求参数为空
     String SYSTEM_USER_ERROR_ADD_FAIL_NAME_NULL = "10002";  //用户名为空
     String SYSTEM_USER_ERROR_ADD_FAIL_NAME_SIZE = "10003";  //用户名长度不对
 
 
-    String SYSTEM_USER_INFO_ADD = "10500";   //用户添加成功
+    //删除
+    String SYSTEM_USER_ERROR_DELETE_FAIL_UID_NULL = "10020";  //uid为空
+
+
+    //修改
+    String SYSTEM_USER_ERROR_UPDATE_FAIL_UID_NULL = "10020";  //uid为空
+
+
+
+
 
 
 

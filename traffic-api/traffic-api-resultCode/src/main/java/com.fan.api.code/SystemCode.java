ef265290c94a1926b4e17ed01381a216a4449a2f
fan <909483306@qq.com>
2022-11-28 02:06:12 +0800

    新增认证管理错误码

--- traffic-api/traffic-api-resultCode/src/main/java/com.fan.api.code/SystemCode.java
+++ traffic-api/traffic-api-resultCode/src/main/java/com.fan.api.code/SystemCode.java
@@ -30,17 +30,29 @@ public interface SystemCode {
     //修改
     String SYSTEM_USER_ERROR_UPDATE_FAIL_UID_NULL = "10020";  //uid为空
 
-
-
-
-
+    //查询
+    String SYSTEM_USER_ERROR_GET_FILE_RESULT_NULL = "10040";  //查询结果为空
+    String SYSTEM_USER_ERROR_GET_FAIL_PARAM_NULL = "10041"; //请求参数为空
 
 
     //角色管理		11000 - 11999
 
     String SYSTEM_ROLE_ERROR_ADD_FAIL = "11000"; //角色添加失败
 
-    //权限管理		12000 - 12999
+    //认证管理		12000 - 12999
+    String SYSTEM_ERROR_AUTHENTICATION_NO_TOKEN = "12000";    //请求不携带token
+    String SYSTEM_ERROR_AUTHENTICATION_FAIL_TOKEN = "12001";    //token验证失败
+    String SYSTEM_ERROR_AUTHENTICATION_PARAM_NULL = "12006"; //请求参数为空
+    String SYSTEM_ERROR_AUTHENTICATION_USER_NOTFOUND = "12002"; //用户未找到
+    String SYSTEM_ERROR_AUTHENTICATION_USERNAME_NULL = "12003"; //用户名为空
+    String SYSTEM_ERROR_AUTHENTICATION_USERPASS_NULL = "12004"; //用户名为空
+    String SYSTEM_ERROR_AUTHENTICATION_USERPASS_ERROR = "12005"; //用户名或密码错误
+
+
+
+
+
+
 
 
 
